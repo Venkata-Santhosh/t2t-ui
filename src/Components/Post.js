c2a4c2ea1c04b0f763a7143dc2bc4d0a06c13128
VenkataSanthosh <piduri.santhosh@gmail.com>
2018-05-05 22:58:53 -0500

    Space added

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -6,6 +6,7 @@ const post = (props) => (
     <div className="card card-custom " onClick = {props.clicked} >
         <div className="card-body">
             <img src={props.imageURL} className="img-fluid card-img-heigh"/>
+            
             <h3 className="card-title">{props.title}</h3>
             <h5 className="card-subtitle">{props.description}</h5>
         </div>
